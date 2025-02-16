1d02c9647ed6b29ead2a8010838f204bc3fe555a
DewiKresnawati <dewidesember20@gmail.com>
2025-02-16 13:32:57 +0700

    connect

--- config/database.go
+++ config/database.go
@@ -20,12 +20,12 @@ func ConnectDB() error {
 
 	// Ambil variabel dari .env
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
-	)
+        os.Getenv("DB_USER"),
+        os.Getenv("DB_PASSWORD"),
+        os.Getenv("DB_HOST"),
+        os.Getenv("DB_PORT"),
+        os.Getenv("DB_NAME"),
+    )
 
 	// Koneksi ke database
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
